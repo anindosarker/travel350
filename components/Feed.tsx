fd39bc54a3ea375e1cdd51cc975f03b882a0f0c6
anindo <sarkeranindo@gmail.com>
2022-11-13 00:05:20 +0600

    Not fixed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client'
 import React from 'react'
 import { GET_ALL_POST } from '../graphql/queries'
+import Post from './Post';
 
 function Feed() {
 
@@ -9,12 +10,16 @@ function Feed() {
   const posts: Post[] = data?.getPostList;
 
   console.log("post", posts,"data", data,"error", error);
-  
 
   
   return (
-    <div>Feed</div>
-  )
+    <div>
+      Feed
+      {posts?.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
 
 export default Feed
---
+++ components/Post.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+type Props = {
+  post: Post;
+};
+
+function Post({ post }: Props) {
+  return <div>Post</div>;
+}
+
+export default Post
