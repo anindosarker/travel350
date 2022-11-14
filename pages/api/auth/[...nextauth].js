8566b30853fcdf07d62d1dfa79ad65ebfe7798b0
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2022-11-14 19:26:03 +0600

    credential auth added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import RedditProvider from "next-auth/providers/reddit";
+import CredentialProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -19,7 +20,53 @@ export const authOptions = {
       clientId: process.env.REDDIT_CLIENT_ID,
       clientSecret: process.env.REDDIT_CLIENT_SECRET,
     }),
+    CredentialProvider({
+      name: "credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "johndoe@test.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: (credentials) => {
+        //database look up here
+        if (
+          credentials.username === "john" &&
+          credentials.password === "test"
+        ) {
+          return {
+            id: 2,
+            name: "John",
+            email: "johndoe@test.com",
+          };
+        }
+        // login failed
+        return null;
+      },
+    }),
   ],
+  callbacks: {
+    jwt: ({ token, user }) => {
+      //first time user auth
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (token) {
+        session.id = token.id;
+      }
+      return session;
+    },
+  },
+  secret: "test",
+  jwt: {
+    secret: "test",
+    encryption: true,
+  },
 };
 
 export default NextAuth(authOptions);
