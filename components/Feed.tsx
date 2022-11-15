0ef0285e93043fd391f245d56814e0fdd48d9d43
anindo <sarkeranindo@gmail.com>
2022-11-15 19:17:19 +0600

    graphql queries folder added, working

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,23 +1,21 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { GET_POST_LIST } from "../graphql/queries";
+import Post from "./Post";
 
-const GET_QUERY = gql`
-  query MyQuery {
-    getPostList {
-      created_at
-      description
-      end_date
-    }
-  }
-`;
 
 function Feed() {
-  const { loading, error, data } = useQuery(GET_QUERY);
+  const { loading, error, data } = useQuery(GET_POST_LIST);
 
   if (loading) return <p>Loading ...</p>;
 
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return(<div>
+    <Post/>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  </div> 
+    );
+
 }
 
 export default Feed;
--- components/Post.tsx
+++ components/Post.tsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/24/solid";
+import React from "react";
+import Avatar from "./Avatar";
 
 
-type Props = {
-  post: Post;
-};
 
-function Post({ post }: Props) {
-  return <div>Post</div>;
+
+
+function Post() {
+  return (
+    <div>
+      {/* <div>Post Title</div> */}
+
+
+      <div>Post Title</div>
+      
+      
+      
+      {/* <div>location</div> */}
+      <div>location</div>
+      
+      
+      
+      {/* <div>Posted by</div> */}
+      <div>Posted by</div>
+      
+      
+      {/* <div>Start Date</div> */}
+      <div>Start Date</div>
+
+
+
+
+      {/* <div>End Date</div> */}
+      <div>End Date</div>
+
+
+
+      {/* <div>Details</div> */}
+      <div>Details</div>
+
+
+
+
+
+    </div>
+  );
 }
 
-export default Post
+export default Post;
