2390b3f1efd6a7f5716a96e6220427c83a210085
Noor <iftekhar3927@gmail.com>
2022-11-16 03:26:13 +0600

    places working, types added, query added

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -23,3 +23,18 @@ export const GET_POST_LIST = gql`
     }
   }
 `;
+
+export const GET_PLACES_LIST = gql`
+  query MyQuery {
+    getPlacesList {
+      city {
+        name
+      }
+      city_id
+      created_at
+      description
+      id
+      name
+    }
+  }
+`;
--- pages/places.tsx
+++ pages/places.tsx
@@ -1,37 +1,30 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { GET_PLACES_LIST } from "../graphql/queries";
 
-const GET_QUERY = gql`
-  query MyQuery {
-    getPostList {
-      created_at
-      description
-      end_date
-      id
-      place_id
-      start_date
-      title
-      user_id
-      comment {
-        text
-      }
-      places {
-        name
-        city {
-          name
-        }
-      }
-    }
-  }
-`;
+
 
 function places() {
-  const { loading, error, data } = useQuery(GET_QUERY);
+  const { loading, error, data } = useQuery(GET_PLACES_LIST);
+
+  const cities: City[] = data?.getPlacesList;
 
   if (loading) return <p>Loading ...</p>;
 
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return (
+    <div>
+      {cities?.map((city) => (
+        <div className="flex items-center ">
+          <div key={city.id} className="bg-white  text-9xl">
+            {city.name}
+          </div>
+        </div>
+      ))}
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default places;
