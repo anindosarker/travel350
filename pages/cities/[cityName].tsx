698e38b6d8ac119caccc4c92fa6582946ac8b27d
anindo <sarkeranindo@gmail.com>
2022-11-23 02:32:25 +0600

    city working

--- pages/cities/[cityName].tsx
+++ pages/cities/[cityName].tsx
@@ -2,18 +2,35 @@ import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import React from "react";
 import { GET_CITY_BY_NAME } from "../../graphql/queries";
+import Feed from "../../components/Feed";
 
 function CityPage() {
-  const router = useRouter();
+  const {
+    query: { cityName },
+  } = useRouter();
 
   const { data, loading, error } = useQuery(GET_CITY_BY_NAME, {
     variables: {
-      id: router.query.cityName,
+      name: cityName,
     },
   });
+
+  const places: Places[] = data?.getCityByCityName?.places;
+  console.log(places);
+  
+
+
+
+  if (loading) return <p>Loading ...</p>;
+
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+
   return (
     <div>
-      <div>Welcome to {data?.getCityByCityName.name} City Page</div>
+      <div>Welcome to {cityName} City Page</div>
+      {places?.map((place) => (
+      <Feed key={place.id} placeName={place.name as string}/>
+      ))}
     </div>
   );
 }
