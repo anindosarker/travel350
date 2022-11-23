ec2aae1e36a2dfb1caf4eec5f57a5895783b472b
anindo <sarkeranindo@gmail.com>
2022-11-24 05:02:40 +0600

    dbase changed

--- typings.d.ts
+++ typings.d.ts
@@ -1,17 +1,18 @@
 type Post = {
-  comment: Comment[]
-  created_at: string
-  description: string
-  end_date: string
-  id: number
-  place_id: number
-  places: Places
-  start_date: string
-  title: string
-  user_id: number
-  usertable: Usertable
-  vote: Vote[]
-}
+  comment: Comment[];
+  created_at: string;
+  description: string;
+  end_date: string;
+  id: number;
+  place_id: number;
+  places: Places;
+  start_date: string;
+  title: string;
+  user_id: number;
+  usertable: Usertable;
+  vote: Vote[];
+  image: string;
+};
 
 type City = {
   created_at: string
