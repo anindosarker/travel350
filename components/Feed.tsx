22f8f5c31c52d8e99e48a5750e0790e272e476c9
Farhan Mahtab Mahi <farhan.mahi1999@gmail.com>
2022-11-24 14:34:15 +0600

    minor change

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -14,7 +14,12 @@ function Feed({ placeName }: Props) {
     ? useQuery(GET_POST_LIST)
     : useQuery(GET_PLACES_BY_NAME, { variables: { name: placeName } });
 
-  if (loading) return <p className="text-center text-4xl m-6 mt-20 uppercase tracking-widest">Loading ...</p>;
+  if (loading)
+    return (
+      <p className="text-center text-4xl m-6 mt-20 uppercase tracking-widest">
+        Loading ...
+      </p>
+    );
 
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
 
@@ -22,10 +27,17 @@ function Feed({ placeName }: Props) {
     ? data?.getPostList
     : data?.getPlacesByPlaceName?.post;
 
-
   return (
     <div>
-      {placeName ? <div className="text-center text-4xl m-6 mt-20 uppercase tracking-widest">Feed for {placeName}</div> : <div className="text-center text-4xl m-6 mt-20 uppercase tracking-widest">Feed</div>}
+      {placeName ? (
+        <div className="text-center text-3xl m-6 mt-20 uppercase tracking-widest">
+          Feed for {placeName}
+        </div>
+      ) : (
+        <div className="text-center text-4xl m-6 mt-20 uppercase tracking-widest">
+          Feed
+        </div>
+      )}
       {posts?.map((post) => (
         <Post key={post.id} post={post} />
       ))}
