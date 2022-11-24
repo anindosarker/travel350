1b120fa23feceebc0f4139a567dd75139a12b5bb
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2022-11-24 13:28:32 +0600

    userExists fixed

--- components/Searchbox.tsx
+++ components/Searchbox.tsx
@@ -2,15 +2,34 @@ import React from "react";
 import homebg from "../public/homeBackground.jpg";
 import Image from "next/image";
 import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/solid";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  searchbox: string;
+};
+
+
+
 const Searchbox = () => {
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const [search, setSearch] = useState("");
+
   return (
     <div className="scale-110 m-auto ">
-      <form action="" className="w-full max-w-sm" >
+      <form onSubmit={onSubmit} className="w-full max-w-sm" >
         <div className="relative flex items-center text-black ">
           <MagnifyingGlassCircleIcon className="w-7 absolute ml-1 pointer-events-none " />
           <input
-            type="text"
-            name="search"
+            type="search"
+            {...register("searchbox")}
+           
             placeholder="Search"
             autoComplete="on"
             aria-aria-label="Search"
