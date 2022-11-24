9c0fb56639f37f02ea014a5fe51ab600817054c2
anindo <sarkeranindo@gmail.com>
2022-11-24 14:23:36 +0600

    places page links

--- components/City.tsx
+++ components/City.tsx
@@ -15,32 +15,30 @@ function City() {
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-center mt-24 mr-28">
-        <Searchbox/>
+        <Searchbox />
       </div>
       <div className="max-w-[1240px] mx-auto">
-      <p className="text-center text-4xl m-6 mt-20 uppercase tracking-widest">
-        browse by cities
-      </p>
-      <div className="grid md:grid-cols-6 grid-cols-2 gap-4">
-        {cities?.map((city) => (
-          <div className="w-full">
-            <div className="max-w-[680px] mx-auto px-2 py-2   ">
-              <Link href="../pages/cities/[cityposts].tsx">
-              <div
-                key={city.id}
-                className="bg-gray-50 text-center  text-lg p-4 rounded-lg m-4 shadow-xl hover:bg-white ease-in duration-100 border-gray-500 border-b md:text-xl  "
-              >
-                {city.name}
+        <p className="text-center text-4xl m-6 mt-20 uppercase tracking-widest">
+          browse by cities
+        </p>
+        <div className="grid md:grid-cols-6 grid-cols-2 gap-4">
+          {cities?.map((city) => (
+            <div className="w-full">
+              <div className="max-w-[680px] mx-auto px-2 py-2   ">
+                <Link href={`/cities/${city?.name}`}>
+                  <div
+                    key={city.id}
+                    className="bg-gray-50 text-center  text-lg p-4 rounded-lg m-4 shadow-xl hover:bg-white ease-in duration-100 border-gray-500 border-b md:text-xl  "
+                  >
+                    {city.name}
+                  </div>
+                </Link>
               </div>
-              </Link>
-              
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
-    </div>
-    
   );
 }
 
--- components/Place.tsx
+++ components/Place.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@apollo/client";
+import Link from "next/link";
 import { GET_CITY_LIST, GET_PLACES_LIST } from "../graphql/queries";
 
 function Place() {
@@ -19,12 +20,14 @@ function Place() {
         {cities?.map((city) => (
           <div className="w-full">
             <div className="max-w-[680px] mx-auto px-2 py-2   ">
-              <div
-                key={city.id}
-                className="bg-gray-50 text-center  text-lg p-4 rounded-lg m-4 shadow-xl hover:bg-white ease-in duration-100 border-gray-500 border-b md:text-xl  "
-              >
-                {city.name}
-              </div>
+              <Link href={`/places/${city?.name}`}>
+                <div
+                  key={city.id}
+                  className="bg-gray-50 text-center  text-lg p-4 rounded-lg m-4 shadow-xl hover:bg-white ease-in duration-100 border-gray-500 border-b md:text-xl  "
+                >
+                  {city.name}
+                </div>
+              </Link>
             </div>
           </div>
         ))}
